271481409bfdb6902a3ef36631fc05344dda26d2
Olga Rudson <o.rudson@mail.ru>
2022-10-17 11:38:27 +0300

    Третья цифра числа

---
+++ example002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine() ??"";
+int number = int.Parse(numberStr);
+
+
+ if ( number > 99 )
+{
+  Console.WriteLine(number.ToString()[2]);
+}
+else
+{
+  Console.Write("В этом числе нет третьей цифры");
+}
