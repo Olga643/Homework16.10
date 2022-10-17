7899a9b61ebf74ac5498432fe76a4ab45d32a269
Olga Rudson <o.rudson@mail.ru>
2022-10-17 11:47:25 +0300

    Проверка выходного дня

--- example003/Program.cs
+++ example003/Program.cs
@@ -7,11 +7,15 @@ Console.WriteLine ("Ведите число от 1 до 7");
 string numberStr = Console.ReadLine() ??"";
 int number = int.Parse(numberStr);
 
-if ( number > 5 )
+if ( number > 5 && number <= 7)
 { 
     Console.WriteLine ("Ура! Выходной");
-}    
-else 
+}   
+else if(number >= 1 && number <= 5)
 {
     Console.WriteLine ("Будний день");
+} 
+else 
+{
+    Console.WriteLine ("В неделе СЕМЬ дней, введите число от 1 до 7");
 }
