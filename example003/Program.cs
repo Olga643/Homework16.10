e38daa67146ad55ad5734bb9e35824ff8a84f408
Olga Rudson <o.rudson@mail.ru>
2022-10-17 11:40:39 +0300

    Проверка выходного дня

---
+++ example003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine ("Ведите число от 1 до 7");
+string numberStr = Console.ReadLine() ??"";
+int number = int.Parse(numberStr);
+
+if ( number > 5 )
+{ 
+    Console.WriteLine ("Ура! Выходной");
+}    
+else 
+{
+    Console.WriteLine ("Будний день");
+}
